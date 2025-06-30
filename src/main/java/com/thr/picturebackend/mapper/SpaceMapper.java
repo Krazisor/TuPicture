670ad9dc483a0bbbe5c4679ec51b7b68b5ddcea1
haoranTu <1642804440@qq.com>
2025-06-30 17:16:39 +0800

    feat: 空间模块

---
+++ src/main/java/com/thr/picturebackend/mapper/SpaceMapper.java
@@ -0,0 +1,18 @@
+package com.thr.picturebackend.mapper;
+
+import com.thr.picturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 屠皓然
+* @description 针对表【space(空间)】的数据库操作Mapper
+* @createDate 2025-06-30 12:41:26
+* @Entity com.thr.picturebackend.model.entity.Space
+*/
+public interface SpaceMapper extends BaseMapper<Space> {
+
+}
+
+
+
+
---
+++ src/main/java/com/thr/picturebackend/service/SpaceService.java
@@ -0,0 +1,32 @@
+package com.thr.picturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.thr.picturebackend.model.dto.space.SpaceAddRequest;
+import com.thr.picturebackend.model.dto.space.SpaceQueryRequest;
+import com.thr.picturebackend.model.entity.Space;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.thr.picturebackend.model.entity.User;
+import com.thr.picturebackend.model.vo.SpaceVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+* @author 屠皓然
+* @description 针对表【space(空间)】的数据库操作Service
+* @createDate 2025-06-30 12:41:26
+*/
+public interface SpaceService extends IService<Space> {
+
+    void validSpace(Space space, boolean add);
+
+    SpaceVO getSpaceVO(Space space, HttpServletRequest request);
+
+    void fillSpaceBySpaceLevel(Space space);
+
+    Page<SpaceVO> getSpaceVOPage(Page<Space> spacePage, HttpServletRequest request);
+
+    QueryWrapper<Space> getQueryWrapper(SpaceQueryRequest spaceQueryRequest);
+
+    long addSpace (SpaceAddRequest spaceAddRequest, User loginUser);
+}
